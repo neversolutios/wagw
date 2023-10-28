058df2183791da8a936e76a16d4169f9ac665a0c
pesonaprojects <125332842+pesonaprojects@users.noreply.github.com>
2023-10-28 09:08:11 +0700

    Delete API

--- wa-gw/WAGW PRO - API/key.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = 'https://productions.wa-server.com' ;    
-$token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjI2LCJpYXQiOjE2Mzg3NjkxMzMsImV4cCI6MTY3MDMyNjczM30.XCCtA-GwgHQ5g_a_MExaQ7RLzdwrvoRQzg9PT18ZAok' ;
-$deviceid = '10' ; 
-$devicekey = '2352ed5e-92d4-4459-8107-da3679eafda0' ;
-?>
